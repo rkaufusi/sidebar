e5adee1e0267cb6a5e4a0dc57e0fe3491752c76c
rkaufusi <rhettkaufusi@gmail.com>
2022-05-08 11:09:23 -0700

    styled icons

--- src/sideBar.tsx
+++ src/sideBar.tsx
@@ -6,11 +6,10 @@ import {FaFire, FaPoo} from 'react-icons/fa'
 const SideBar = () => {
     return (
         <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-gray-900 text-white shadow-lg">
+            <SideBarIcon icon={<FaFire size="28"/>}/>
             <SideBarIcon icon={<BsPlus size="32"/>}/>
-            <i>B</i>
-            <i>C</i>
-            <i>D</i>
-            <i>E</i>
+            <SideBarIcon icon={<BsFillLightningFill size="20"/>}/>
+            <SideBarIcon icon={<FaPoo size="20"/>}/>
         </div>
     )
 }
